3e4244054cbeb371882b11ae830956d9dec3fd7e
praju-jagtap <prajaktajagtap2412@gmail.com>
2022-10-13 21:58:47 +0530

    [Prajakta] Add: Calculate Fare For Multiple Rides

--- CabInvoiceGeneratorApp/Program.cs
+++ CabInvoiceGeneratorApp/Program.cs
@@ -7,6 +7,10 @@
             CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();  //UC1
             double fare = cabInvoiceGenerator.CalculatFare(2.0, 4.0);
             Console.WriteLine("The Total Fare For The Journey : " + fare);
+
+            Ride[] ride = { new Ride(1, 2), new Ride(3, 4) };                               //UC2
+            double getMultiple = cabInvoiceGenerator.GetMultipleRideFare(ride);
+            Console.WriteLine("\nThe Aggregate Total For All : " + getMultiple);
         }
     }
 }
